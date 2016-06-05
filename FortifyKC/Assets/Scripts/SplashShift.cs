6f58cf88bb36334b6115f8a5e77402f0ab0ed825
Katricia Herring <kherring@nd.edu>
2016-06-05 14:41:35 -0500

    update logo

--- FortifyKC/Assets/Scripts/SplashShift.cs
+++ FortifyKC/Assets/Scripts/SplashShift.cs
@@ -5,11 +5,16 @@ public class SplashShift : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        float seconds = 4;
+        while (seconds > 0) {
+            seconds -= Time.deltaTime;
+        }
+        NextLevelButton next = new NextLevelButton();
+        next.nextLevel(1);
+    }
 }
