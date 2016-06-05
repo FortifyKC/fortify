98ebd59a5abd430f9693871643303138e021faf6
Jace <jcork2@mst.edu>
2016-06-05 11:37:56 -0500

    Jake asked me to do this

--- FortifyKC/Assets/Scripts/CameraSetup.cs
+++ FortifyKC/Assets/Scripts/CameraSetup.cs
@@ -6,8 +6,14 @@ public class CameraSetup : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Camera cm = Camera.main;
-		cm.orthographicSize = Screen.width / 2;
+		Camera cm = gameObject.GetComponent<Camera> ();
+		if (cm) {
+			cm.orthographicSize = Screen.width / 2;
+
+			RectTransform cmRect = cm.transform as RectTransform;
+			cmRect.anchoredPosition = new Vector2 (	Screen.width,
+													Screen.height);
+		}
 
 	}
 	
