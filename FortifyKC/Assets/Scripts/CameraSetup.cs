3690e8aefd986bc55aa193b2017cedfe34ad9350
Jace <jcork2@mst.edu>
2016-06-05 11:18:43 -0500

    Some more work I guess

---
+++ FortifyKC/Assets/Scripts/CameraSetup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CameraSetup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera cm = Camera.main;
+		cm.orthographicSize = Screen.width / 2;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
